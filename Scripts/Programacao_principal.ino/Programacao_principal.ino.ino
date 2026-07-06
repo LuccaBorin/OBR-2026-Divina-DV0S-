29123fde03f8d6a16329b62efb1c531ce1a54edf
LuccaBorin <realmente.falando@gmail.com>
2026-07-06 18:28:43 -0300

    Update Programacao_principal.ino.ino

--- Scripts/Programacao_principal.ino/Programacao_principal.ino.ino
+++ Scripts/Programacao_principal.ino/Programacao_principal.ino.ino
@@ -380,13 +380,13 @@ void seguirLinha() {
     case CURVA_LEVE_ESQUERDA:
       // -------- CORREÇÃO SUAVE PARA A ESQUERDA --------
       mover(ESQUERDA, VEL_CURVA, 100);
-      mover(FRENTE, VEL_BASE, 50);
+      mover(FRENTE, VEL_BASE, 25);
       break;
 
     case CURVA_LEVE_DIREITA:
       // -------- CORREÇÃO SUAVE PARA A DIREITA --------
       mover(DIREITA, VEL_CURVA, 100);
-      mover(FRENTE, VEL_BASE, 50);
+      mover(FRENTE, VEL_BASE, 25);
       break;
 
     case NENHUM:
