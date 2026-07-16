48b62e78fa24556495ce9b5b74a235e8c16c6de4
LuccaBorin <realmente.falando@gmail.com>
2026-07-16 14:46:54 -0300

    Update Programacao_principal.ino.ino

--- Scripts/Programacao_principal.ino/Programacao_principal.ino.ino
+++ Scripts/Programacao_principal.ino/Programacao_principal.ino.ino
@@ -433,11 +433,9 @@ void seguirLinha() {
         lerSensores();
         mover(DIREITA, VEL_CURVA, 3);
       }
+      mover(DIREITA, VEL_CURVA, 100);
       mover(PARAR, VEL_BASE, 1000);
-      while (intDistanciaL < 25) {
-        lerSensores();
-        mover(FRENTE, VEL_DEFAULT, 3);
-      }
+      mover(FRENTE, VEL_BASE, 3000);
       mover(PARAR, VEL_BASE, 1000);
       break;
     case INTERSECAO_SEM_MARCACAO:
